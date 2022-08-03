8b5408afc300da18da9c03e8cf7b9967180acde9
21TekSavvvy <jareddarochajj@gmail.com>
2022-08-03 01:29:27 -0700

    Madlibs.py finished

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+name1 = input("Friend's name: ")
+pnoun = input("Plural Noun: ")
+num1 = input("Number: ")
+verb1 = input("Verb ending with ing: ")
+verb2 = input("Verb: ")
+madlibs = f"I've always been a {adj1} kid, but it never stopped me from having friends somehow. I even had a(n) {adj2} friend, {name1} who just adored {pnoun}. Now tell me that wasn't meant to  be because I ADORE! But, our friendship only lasted {num1} days, partly becauseis all we had in common and also because I enjoyed spending time {verb1} and liked to {verb2}. The two don't mix!"
+print(madlibs)
