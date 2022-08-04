081bf884b695ac930fec0e97a84a7185efe278ef
21TekSavvvy <jareddarochajj@gmail.com>
2022-08-03 22:47:52 -0700

    started a new function caalled make_new_board

--- MineSweeper/minesweeper.py
+++ MineSweeper/minesweeper.py
@@ -6,8 +6,15 @@ class Board:
         self.dim_size = dim_size
         self.num_bombs = num_bombs
 
+        self.board.make_new_board()
 
-    
+        self.dug= set()
+
+
+    def make_new_board(self):
+        
+
+
 
 def play(dim_siz = 10, num_bombs = 10):
     pass
