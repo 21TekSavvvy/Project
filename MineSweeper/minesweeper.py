2c9dd3f1db9687335bd5296268f8bcfa3c7eef80
21TekSavvvy <jareddarochajj@gmail.com>
2022-08-13 14:58:45 -0700

    finished print function __str__

--- MineSweeper/minesweeper.py
+++ MineSweeper/minesweeper.py
@@ -75,6 +75,8 @@ class Board:
                 self.dig(r,c)
 
         return True
+
+
     def __str__(self):
 
         visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
@@ -84,6 +86,35 @@ class Board:
                     visible_board[row][col]  = str(self.board[row][col])
                 else:
                     visible_board[row][col] = ' '
+        string_rep=''
+        widths = []
+        for idx in range(self.dim_size):
+            columns = maps(lambda x: x[idx], visible_board)
+            widths. append(len(max(columns, key = len)))
+
+        indices = [i for i in range(self.dim_size)]
+        indices_row = ' '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx])+ "s"
+            cels.append(format % (col))
+        indices_row += ' '.join(cells)
+        indices_row += ' |\n'
+
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + dtr(widths[idx]) + "s"
+                cells.append(format % col)
+            string_rep += ' |'. join(cells)
+            string_rep +=  ' |\n'
+
+        str_len = int(len(string_rep)/self.dim_size)
+        string_rep = indices_row + '-'*str_len + '/n' + string_rep + '-'*str_len
+
+        return string_rep
 
 
 
